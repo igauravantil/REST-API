9339fcbdc8f5a3f2d6e5ca54705793e4215fde5f
thegauravantil <gauravsatila@gmail.com>
2021-03-21 20:01:28 +0530

    added model and some changes

---
+++ models/post.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const {Schema} = mongoose;
+
+const postSchema = new Schema({
+    name : String,
+    id : Number,
+    Content: {
+        type :String ,
+        
+    },
+    comments:String,
+    Date:{
+        type : Date,
+        default:Date.now
+    }
+})
+const post = mongoose.model('post',postSchema);
+
+module.exports = post
